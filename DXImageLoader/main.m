ef1d6c79bef4f1f50a10cc79f5c02516e159d167
x6doooo <x6doooo@gmail.com>
2014-08-07 14:52:48 +0800

    new

---
+++ DXImageLoader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DXImageLoader
+//
+//  Created by Dx.Yang on 14-8-7.
+//  Copyright (c) 2014年 Dx.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DXAppDelegate class]));
+    }
+}
