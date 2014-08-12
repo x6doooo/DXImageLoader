83873c9fbe88e930223fb31cafdc46d67d60b9e6
x6doooo <x6doooo@gmail.com>
2014-08-12 11:37:22 +0800

    .

---
+++ DXImageLoader/DXImageLoader/DXImageScrollView.h
@@ -0,0 +1,13 @@
+//
+//  DXImageScrollView.h
+//  DXImageLoader
+//
+//  Created by Dx.Yang on 14-8-11.
+//  Copyright (c) 2014年 Dx.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXImageScrollView : UIScrollView
+
+@end
---
+++ DXImageLoader/DXImageLoader/DXImageScrollView.m
@@ -0,0 +1,13 @@
+//
+//  DXImageScrollView.m
+//  DXImageLoader
+//
+//  Created by Dx.Yang on 14-8-11.
+//  Copyright (c) 2014年 Dx.Yang. All rights reserved.
+//
+
+#import "DXImageScrollView.h"
+
+@implementation DXImageScrollView
+
+@end
