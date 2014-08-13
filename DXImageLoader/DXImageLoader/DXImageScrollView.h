540412dbed56c85958b92f8dc6ff0523dd624cb0
x6doooo <x6doooo@gmail.com>
2014-08-13 09:52:00 +0800

    .

--- DXImageLoader/DXImageLoader/DXImageScrollView.h
+++ DXImageLoader/DXImageLoader/DXImageScrollView.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DXUIScrollView.h"
 
-@interface DXImageScrollView : UIScrollView
-
+@interface DXImageScrollView : UIView <DXUIScrollViewDelegate>
+{
+    UIImageView *theImage;//背景图
+    DXUIScrollView *theScroll;//控制器
+    CGFloat currentScale;//当前倍率
+    CGFloat minScale;//最小倍率
+}
+@property(nonatomic) float maxScale;
+- (void)setImage:(UIImage *)image;
+-(void)singleGesture:(UIGestureRecognizer *)sender;
+-(void)doubleGesture:(UIGestureRecognizer *)sender;
 @end
--- DXImageLoader/DXImageLoader/DXImageScrollView.m
+++ DXImageLoader/DXImageLoader/DXImageScrollView.m
@@ -8,6 +8,133 @@
 
 #import "DXImageScrollView.h"
 
+
 @implementation DXImageScrollView
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        _maxScale = 2.0;
+        minScale = 1.0;
+        currentScale = 1.0;
+
+        theScroll = [[DXUIScrollView alloc] initWithFrame:frame];
+        theScroll.userInteractionEnabled = YES;
+        theScroll.maximumZoomScale = 2.0;
+        theScroll.minimumZoomScale = 1.0;
+        theScroll.decelerationRate = 1.0;
+        theScroll.delegate = self;
+        theScroll.imageScrollViewDelegate = self;
+        theScroll.backgroundColor = [UIColor blackColor];
+        theScroll.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin|
+                                     UIViewAutoresizingFlexibleTopMargin|
+                                     UIViewAutoresizingFlexibleHeight;
+
+        [self addSubview:theScroll];
+        
+        theImage = [[UIImageView alloc] init];
+        
+        //图片
+        //UIImage *theImageName=[UIImage imageNamed:imageName];
+        //theImage=[[UIImageView alloc] initWithImage:theImageName];
+        theImage.userInteractionEnabled=YES;
+        theImage.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleHeight;
+        //图片默认作为2倍图处理
+        
+        [theScroll addSubview:theImage];
+        /*
+        //单击
+        UITapGestureRecognizer *singleGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleGesture:)];
+        singleGesture.numberOfTapsRequired = 1;
+        [theImage addGestureRecognizer:singleGesture];
+        //双击手势
+        UITapGestureRecognizer *doubelGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleGesture:)];
+        doubelGesture.numberOfTapsRequired = 2;
+        [theImage addGestureRecognizer:doubelGesture];
+        
+        [singleGesture requireGestureRecognizerToFail:doubelGesture];
+        */
+        
+        //other
+    }
+    return self;
+}
+
+
+-(void)singleGesture:(UIGestureRecognizer *)sender
+{
+    NSLog(@"1111");
+    [self removeFromSuperview];
+}
+
+- (void)setImage:(UIImage *)image
+{
+    //UIImage *_image = [UIImage imageWithData:UIImageJPEGRepresentation(image, 1)];
+    //theImage = [[UIImageView alloc] initWithImage:_image];
+    theImage.image = image;
+    //NSLog(@"%f, %f", self.frame.size.width, self.frame.size.height);
+
+    //theImage.frame = CGRectMake(0, 0, image.size.width/2, image.size.height/2);
+    //[theScroll setContentSize:CGSizeMake(image.size.width/2, image.size.height/2)];
+    theImage.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
+    theImage.contentMode = UIViewContentModeScaleAspectFit;
+    [theScroll setContentSize:CGSizeMake(self.frame.size.width, self.frame.size.height)];
+}
+
+#pragma mark -setter action
+-(void)setMaxScale:(float)maxScale
+{
+    _maxScale=maxScale;
+    theScroll.maximumZoomScale=maxScale;
+}
+
+#pragma mark -UIScrollView delegate
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return theImage;
+}
+
+-(void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale
+{
+    currentScale=scale;
+}
+#pragma mark -DoubleGesture Action
+-(void)doubleGesture:(UIGestureRecognizer *)sender
+{
+    
+    //当前倍数等于最大放大倍数
+    //双击默认为缩小到原图
+    if (currentScale == _maxScale) {
+        currentScale = minScale;
+        [theScroll setZoomScale:currentScale animated:YES];
+        return;
+    }
+    //当前等于最小放大倍数
+    //双击默认为放大到最大倍数
+    if (currentScale == minScale) {
+        currentScale = _maxScale;
+        [theScroll setZoomScale:currentScale animated:YES];
+        return;
+    }
+    
+    CGFloat aveScale = minScale + (_maxScale - minScale) / 2.0;//中间倍数
+    
+    //当前倍数大于平均倍数
+    //双击默认为放大最大倍数
+    if (currentScale >= aveScale) {
+        currentScale = _maxScale;
+        [theScroll setZoomScale:currentScale animated:YES];
+        return;
+    }
+    
+    //当前倍数小于平均倍数
+    //双击默认为放大到最小倍数
+    if (currentScale < aveScale) {
+        currentScale = minScale;
+        [theScroll setZoomScale:currentScale animated:YES];
+        return;
+    }
+}
 
 @end
