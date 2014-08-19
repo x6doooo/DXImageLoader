ec5c32fdfadbc6fce29835df4ed4033a67cb22f8
x6doooo <x6doooo@gmail.com>
2014-08-19 13:31:22 +0800

    .

--- DXImageLoader/DXAppDelegate.m
+++ DXImageLoader/DXAppDelegate.m
@@ -9,6 +9,8 @@
 #import "DXAppDelegate.h"
 #import "DXImageLoaderView.h"
 #import "DXImageScrollView.h"
+#import <CommonCrypto/CommonDigest.h>
+
 @implementation DXAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -50,8 +52,23 @@
     [scaleViewButton setTitle:@"scale" forState:UIControlStateNormal];
     [self.window addSubview:scaleViewButton];
     
-    [scaleViewButton addTarget:self action:@selector(showme:)
+    [scaleViewButton addTarget:self
+                        action:@selector(showme:)
               forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    // md5 test
+    NSString *str = @"abc";
+    const char *cStr = [str UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5( cStr, strlen(cStr), digest );
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    NSLog(@"%@", output);
     
     return YES;
 }
